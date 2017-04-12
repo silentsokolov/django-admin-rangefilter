ebed7d0e544802e27aa776106fea9f9b14ddda6f
silentsokolov <silentsokolov@gmail.com>
2017-04-12 23:10:53 +0300

    Prepare 0.3.0

--- rangefilter/__init__.py
+++ rangefilter/__init__.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 
 __author__ = 'Dmitriy Sokolov'
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 
 
 default_app_config = 'rangefilter.apps.RangeFilterConfig'
