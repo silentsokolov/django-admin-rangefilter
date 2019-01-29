07272b7746418c8a8a9fc27b653426e4e4ae1034
Daniel Ruiz <druizca@mercadona.es>
2019-01-29 18:02:38 +0100

    Add template config to Django test settings

--- runtests.py
+++ runtests.py
@@ -23,6 +23,21 @@ settings.configure(
     TEST_RUNNER='django.test.runner.DiscoverRunner',
     USE_TZ=True,
     TIME_ZONE='UTC',
+    TEMPLATES=[
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                ],
+            },
+        },
+    ]
 )
 
 django.setup()
