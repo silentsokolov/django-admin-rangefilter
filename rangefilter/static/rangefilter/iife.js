fd08d670615a4da52d3a196935f84aab478adc09
jsumnerPhD <jonathonresumner@gmail.com>
2019-07-03 09:11:18 -0400

    Clean-up js a bit

--- rangefilter/static/rangefilter/iife.js
+++ rangefilter/static/rangefilter/iife.js
@@ -4,13 +4,13 @@
     function(){
       var form_id = django.jQuery(this).find("form").attr('id').slice(0,-5);
       var qs_name = form_id+"-query-string";
+      var query_string = django.jQuery('input#'+qs_name).val();
       var form_name = form_id+"-form";
 
       // Bind submit buttons
       django.jQuery(this).find("input[type=select]").bind("click",
         function(event){
           event.preventDefault();
-          var query_string = django.jQuery('input#'+qs_name).val();
           var form_data = django.jQuery('#'+form_name).serialize();
           window.location = window.location.pathname + query_string + '&' + form_data;
       });
@@ -18,10 +18,7 @@
       // Bind reset buttons
       django.jQuery(this).find("input[type=reset]").bind("click",
         function(){
-          var query_string = django.jQuery('input#'+qs_name).val();
           window.location = window.location.pathname + query_string;
       });
-
     });
-
 })();
