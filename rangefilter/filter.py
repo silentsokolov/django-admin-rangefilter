a2d87493949e147c6a8e5829a2f1adbaa892eec9
Daniel Hahler <git@thequod.de>
2021-06-24 11:16:10 +0200

    Add stacklevel to DeprecationWarning

--- rangefilter/filter.py
+++ rangefilter/filter.py
@@ -6,7 +6,8 @@ import warnings
 
 warnings.warn(
     'Import from the `filter` module is deprecated. Use `filters` module.',
-    DeprecationWarning
+    DeprecationWarning,
+    stacklevel=2,
 )
 
 from .filters import DateRangeFilter, DateTimeRangeFilter
