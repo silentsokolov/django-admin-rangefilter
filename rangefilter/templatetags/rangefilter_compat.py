74a3ab915906cd7bd127f5345d3fe4a9d228cd88
Mikko Harju <mikko@taiste.fi>
2022-08-10 07:55:44 +0300

    Use admin CSS vars for colors. Use defaults for django < 3.2

--- rangefilter/templatetags/rangefilter_compat.py
+++ rangefilter/templatetags/rangefilter_compat.py
@@ -18,3 +18,17 @@ register = Library()
 @register.simple_tag()
 def static(path):
     return _static(path)
+
+
+CSS_VARIABLES = """
+    :root {
+      --button-bg: #79aec8;
+      --button-fg: #fff;
+      --border-bottom: #eaeaea;
+    }
+"""
+
+
+@register.simple_tag()
+def default_css_vars_if_needed():
+    return CSS_VARIABLES if django.VERSION[:2] < (3, 2) else ""
