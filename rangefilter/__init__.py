91bdefef1065b9ab0efb96e99f918c10ccf5ff76
Dmitriy Sokolov <silentsokolov@gmail.com>
2025-12-13 09:56:14 +0200

    Prepare 0.13.5

--- rangefilter/__init__.py
+++ rangefilter/__init__.py
@@ -3,7 +3,7 @@
 import django
 
 __author__ = "Dmitriy Sokolov"
-__version__ = "0.13.4"
+__version__ = "0.13.5"
 
 if django.VERSION < (3, 2):
     default_app_config = "rangefilter.apps.RangeFilterConfig"
